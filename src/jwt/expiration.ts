cb79c72a03bfc227cb0aabeb0f62e21db0943b06
Nils Ponsard <nilsponsard@gmail.com>
2022-09-27 23:52:07 +0200

    feat: jwt expiration

---
+++ src/jwt/expiration.ts
@@ -0,0 +1,2 @@
+export const ACCESS_EXPIRATION = 60 * 60 * 24; // 1  day
+export const REFRESH_EXPIRATION = 60 * 60 * 24 * 30; // 30 days
--- src/jwt/token.ts
+++ src/jwt/token.ts
@@ -1,10 +1,21 @@
-import { create, verify } from 'https://deno.land/x/djwt@v2.7/mod.ts';
+import { create, getNumericDate, verify } from 'https://deno.land/x/djwt@v2.7/mod.ts';
 import { config } from '../../config.ts';
+import { ACCESS_EXPIRATION, REFRESH_EXPIRATION } from './expiration.ts';
+
+export async function newTokenPair() {
+  const access = await newToken(ACCESS_EXPIRATION);
+  const refresh = await newToken(REFRESH_EXPIRATION);
+  return { access, refresh };
+}
 
 // Create a new token and return it and the uuid
-export async function newToken() {
+export async function newToken(expSeconds: number) {
   const uuid = crypto.randomUUID();
-  const token = await create({ alg: 'ES384', typ: 'JWT' }, { id: uuid }, config.jwt_pair.priv);
+  const token = await create(
+    { alg: 'ES384', typ: 'JWT' },
+    { id: uuid, exp: getNumericDate(expSeconds) },
+    config.jwt_pair.priv,
+  );
 
   return { token, uuid };
 }
